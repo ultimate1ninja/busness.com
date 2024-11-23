42d90db538fec668f34633a761b723b10a2ed84b
ultimate1ninja <aw1156872@gmail.com>
2024-11-23 20:34:01 +0500

    Delete log_ip.php

--- log_ip.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Retrieve the JSON data sent from the HTML page
-$data = json_decode(file_get_contents("php://input"));
-$ip = $data->ip;
-
-// Append the IP address to the log file
-file_put_contents("ips.txt", "IP Address: $ip\n", FILE_APPEND);
-?>
